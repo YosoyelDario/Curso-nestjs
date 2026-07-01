4d10374897b379915c5f7030d9f03e181fd9dbd4
Dario Joaquin Fuentes Ponce <dariofp123@gmail.com>
2026-07-01 15:51:51 -0400

    Hasta Clase 12- Configurar TypeOrm, variables de entorno en .env . actualizacion de modulos y docker

--- my-first-api/src/app.module.ts
+++ my-first-api/src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Env } from './env.model';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService<Env>) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST', { infer: true }),
+        port: configService.get('POSTGRES_PORT', { infer: true }),
+        username: configService.get('POSTGRES_USER', { infer: true }),
+        password: configService.get('POSTGRES_PASSWORD', { infer: true }),
+        database: configService.get('POSTGRES_DB', { infer: true }),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- my-first-api/src/users/users.module.ts
+++ my-first-api/src/users/users.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService], //En caso de que otro modulo quiera utilizar el usersService
+})
 export class UsersModule {}
